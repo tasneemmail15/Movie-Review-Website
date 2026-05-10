eb235b774cefab88791e18128c52a6936448dd0b
tasneemmail15 <tasneemmail15@gmail.com>
2026-05-10 21:05:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+
+using SMS.Web;
+using SMS.Data.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddCookieAuthentication();
+
+// add the movie service to the container
+builder.Services.AddSingleton<IMovieService, MovieServiceDb>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+} 
+else
+{
+    // in development mode seed the database each time the application starts
+    ServiceSeeder.Seed(app.Services.CreateScope().ServiceProvider.GetRequiredService<IMovieService>());  
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
